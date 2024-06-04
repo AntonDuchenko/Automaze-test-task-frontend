19146cece9c3ee81331f27757ff8501356ce0f09
Anton Duchenko <deragandi0@gmail.com>
2024-06-04 11:41:43 +0300

    implement debounce in input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,9 @@ import { useAppSelector } from "@/lib/hooks";
 import { Statuses } from "@/types/Statuses";
 import { filteredTodos } from "@/utils/filteredTodos";
 import { getSearchWith } from "@/utils/searchHepler";
+import debounce from "lodash.debounce";
 import { useRouter, useSearchParams } from "next/navigation";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, Suspense } from "react";
 
 export default function Home() {
   const searchParams = useSearchParams();
@@ -23,49 +24,59 @@ export default function Home() {
 
   const handleOnSelect = (e: ChangeEvent<HTMLSelectElement>) => {
     const newSearchParams = getSearchWith(searchParams, {
-      status: e.target.value || null,
+      status: e.target.value === Statuses.All ? null : e.target.value,
     });
 
     route.push(`?${newSearchParams?.toString()}`);
   };
 
-  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleOnChange = debounce((e: ChangeEvent<HTMLInputElement>) => {
     const newSearchParams = getSearchWith(searchParams, {
       query: e.target.value || null,
     });
 
     route.push(`?${newSearchParams?.toString()}`);
-  };
+  }, 1000);
 
   const preparedTodos = filteredTodos(todos, status, query, sort, order);
 
   return (
-    <main className="container m-auto mt-3">
-      <div className="flex gap-2 h-[40px] mb-4">
-        <select
-          onChange={handleOnSelect}
-          value={status}
-          name="fitler"
-          id="fitler"
-          className="w-[150px] border rounded-md p-2"
-        >
-          <option value={Statuses.All}>All</option>
-          <option value={Statuses.Completed}>Done</option>
-          <option value={Statuses.Active}>In Progress</option>
-        </select>
+    <Suspense>
+      <main className="container m-auto mt-3">
+        <Suspense>
+          <div className="flex gap-2 h-[40px] mb-4">
+            <Suspense>
+              <select
+                onChange={handleOnSelect}
+                value={status}
+                name="fitler"
+                id="fitler"
+                className="w-[150px] border rounded-md p-2"
+              >
+                <option value={Statuses.All}>All</option>
+                <option value={Statuses.Completed}>Done</option>
+                <option value={Statuses.Active}>In Progress</option>
+              </select>
+            </Suspense>
 
-        <input
-          value={query}
-          onChange={handleOnChange}
-          type="text"
-          placeholder="Search..."
-          className="border w-full rounded-md p-2"
-        />
-      </div>
+            <Suspense>
+              <input
+                defaultValue={query}
+                onChange={handleOnChange}
+                type="search"
+                placeholder="Search..."
+                className="border w-full rounded-md p-2"
+              />
+            </Suspense>
+          </div>
+        </Suspense>
 
-      {loading ? <Loader /> : <TableTodos todos={preparedTodos} />}
+        <Suspense>
+          {loading ? <Loader /> : <TableTodos todos={preparedTodos} />}
+        </Suspense>
 
-      <ModalToCreate />
-    </main>
+        <ModalToCreate />
+      </main>
+    </Suspense>
   );
 }
